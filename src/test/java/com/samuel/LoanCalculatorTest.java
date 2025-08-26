a6c6fcec52817541e8ef4f7a005e8275862c0549
Samuel Peter <samuelakanimo@hotmail.com>
2025-08-26 01:25:46 +0100

    Update Selenium test: headless Chrome, unique user-data-dir, correct page title

--- src/test/java/com/samuel/LoanCalculatorTest.java
+++ src/test/java/com/samuel/LoanCalculatorTest.java
@@ -3,6 +3,7 @@ package com.samuel;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoanCalculatorTest {
@@ -11,11 +12,19 @@ public class LoanCalculatorTest {
     public void testPageLoads() {
         // Automatically setup ChromeDriver using WebDriverManager
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        // Set Chrome options for headless mode and unique user data dir (for CI)
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--user-data-dir=/tmp/chrome-" + System.currentTimeMillis());
+
+        WebDriver driver = new ChromeDriver(options);
         driver.get("file:///C:/Users/samue/Projects/credit-loan-calculator/index.html");
 
         // Basic check: page title contains expected text
-        assert driver.getTitle().contains("Loan Calculator");
+        assert driver.getTitle().contains("Credit Loan Calculator");
 
         driver.quit();
     }
